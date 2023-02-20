4aa78e53a448842c0f528e036ee92240e3c24890
tsukumi <tsukumijima@users.noreply.github.com>
2023-02-20 04:22:20 +0000

    Update: VRAM の節約のため、音声ごとに Whisper のモデルを読み込み、音声認識が終わったタイミングで一旦破棄するように変更 inaSpeechSegmenter 実行時に Out of memory になる問題の対策

--- Aivis.py
+++ Aivis.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-# FutureWarning を抑制する
+# FutureWarning と RuntimeWarning を抑制する
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
 
 # Tensorflow のログを抑制する
 ## ref: https://stackoverflow.com/questions/35911252/disable-tensorflow-debugging-information
@@ -13,7 +14,7 @@ import json
 import stable_whisper
 import torch
 import typer
-from typing import cast
+from typing import Any, cast
 
 from Aivis import __version__
 from Aivis import constants
@@ -27,6 +28,7 @@ app = typer.Typer()
 @app.command()
 def segment(
     model_name: constants.ModelNameType = typer.Option('large-v2', help='Model name.'),
+    force_transcribe: bool = typer.Option(False, help='Force Whisper to transcribe audio files.'),
 ):
     typer.echo('=' * utils.GetTerminalColumnSize())
 
@@ -42,29 +44,6 @@ def segment(
     ## すでに抽出済みのファイルがある場合は音源分離は行われず、すでに抽出済みのファイルを使用する
     voices_files = demucs.ExtractVoices(source_files, constants.PREPARE_SOURCES_DIR)
 
-    # Whisper の推論を高速化し、メモリ使用率を下げて VRAM 8GB でも large-v2 モデルを使用できるようにする
-    # ref: https://qiita.com/halhorn/items/d2672eee452ba5eb6241
-
-    # Whisper の学習済みモデルをロード
-    ## 一旦 CPU としてロードしてから CUDA に変更すると、メモリ使用量が大幅に削減されるらしい…
-    typer.echo('Whisper model loading...')
-    model = stable_whisper.load_model(model_name.value, device='cpu')
-    typer.echo('Whisper model loaded.')
-
-    # 学習済みモデルを JIT にコンパイル
-    typer.echo('Whisper model compiling...')
-    model.encoder = torch.jit.script(model.encoder)  # type: ignore
-    model.decoder = torch.jit.script(model.decoder)  # type: ignore
-    typer.echo('Whisper model compiled.')
-
-    # 推論の高速化 & メモリ使用量の削減
-    typer.echo('Run model.half() ...')
-    _ = model.half()
-    typer.echo('Run model.half() done.')
-    typer.echo('Run model.cuda() ...')
-    _ = model.cuda()
-    typer.echo('Run model.cuda() done.')
-
     for voices_file in voices_files:
         typer.echo('=' * utils.GetTerminalColumnSize())
 
@@ -78,16 +57,48 @@ def segment(
         folder.mkdir(parents=True, exist_ok=True)
         typer.echo(f'Folder {folder} created.')
 
-        # すでに音声認識データ (JSON) がある場合はそのデータを使い、新規の音声認識は行わない
-        finalized_results = []
+        finalized_results: list[dict[str, Any]] = []
         results_json_file = constants.PREPARE_SOURCES_DIR / f'{voices_file.name.split(".")[0]}.json'
-        if results_json_file.exists():
+
+        # すでに音声認識データ (JSON) がある場合はそのデータを使い、新規の音声認識は行わない
+        ## なお、--force-transcribe オプションが指定されている場合は JSON ファイルが存在するかに関わらず音声認識を実行する
+        if results_json_file.exists() and force_transcribe is False:
             typer.echo(f'File {voices_file} already transcribed.')
             with open(results_json_file, mode='r', encoding='utf-8') as f:
                 finalized_results = json.load(f)
 
-        # 音声認識を開始
+        # Whisper で音声認識を実行
         else:
+
+            # Whisper の推論を高速化し、メモリ使用率を下げて VRAM 8GB でも large-v2 モデルを使用できるようにする
+            ## 学習済みモデルのロードには時間がかかるので、本来であれば一度ロードしたモデルを再利用するべきだが、
+            ## そうすると inaSpeechSegmenter の実行時に VRAM 8GB な GPU だと Out of memory になってしまうため、
+            ## やむを得ず毎回モデルをロードしている
+            ## ref: https://qiita.com/halhorn/items/d2672eee452ba5eb6241
+
+            # Whisper の学習済みモデルをロード
+            ## 一旦 CPU としてロードしてから CUDA に変更すると、メモリ使用量が大幅に削減されるらしい…
+            typer.echo('-' * utils.GetTerminalColumnSize())
+            typer.echo('Whisper model loading...')
+            model = stable_whisper.load_model(model_name.value, device='cpu')
+            typer.echo('Whisper model loaded.')
+
+            # 学習済みモデルを JIT にコンパイル
+            typer.echo('Whisper model compiling...')
+            model.encoder = torch.jit.script(model.encoder)  # type: ignore
+            model.decoder = torch.jit.script(model.decoder)  # type: ignore
+            typer.echo('Whisper model compiled.')
+
+            # 推論の高速化 & メモリ使用量の削減
+            typer.echo('Run model.half() ...')
+            _ = model.half()
+            typer.echo('Run model.half() done.')
+            typer.echo('Run model.cuda() ...')
+            _ = model.cuda()
+            typer.echo('Run model.cuda() done.')
+
+            # 音声認識を開始
+            typer.echo('-' * utils.GetTerminalColumnSize())
             typer.echo(f'File {voices_file} transcribing...')
             typer.echo('-' * utils.GetTerminalColumnSize())
             results = model.transcribe(
@@ -104,9 +115,14 @@ def segment(
                 initial_prompt = '次の文章は、アニメの物語の書き起こしです。必ず句読点をつけてください。',
             )
 
+            # GPU のメモリを解放する
+            del model
+            torch.cuda.empty_cache()
+            typer.echo(f'File {voices_file} transcribed.')
+
             # 音声認識結果のタイムスタンプを調整し、ファイナライズする
-            finalized_results = stable_whisper.finalize_segment_word_ts(results)
-            finalized_results = [dict(text = ''.join(i), start = j[0]['start'], end = j[-1]['end']) for i, j in finalized_results]
+            finalized_results_ = stable_whisper.finalize_segment_word_ts(results)
+            finalized_results = [dict(text = ''.join(i), start = j[0]['start'], end = j[-1]['end']) for i, j in finalized_results_]
 
             # 音声認識結果をファイルに出力
             with open(results_json_file, mode='w', encoding='utf-8') as f:
@@ -120,6 +136,10 @@ def segment(
             # 出力先の音声ファイルと書き起こしのテキストファイルのパス
             output_audio_file = folder / f'Voices-{count:04d}.wav'
             output_transcription_file = folder / f'Voices-{count:04d}.txt'
+
+            # 書き起こし結果を下処理し、より最適なテキストにする
+            transcription = prepare.PrepareText(result['text'])
+            typer.echo(f'File {output_audio_file} Transcription: {transcription}')
 
             # 一文ごとに切り出した (セグメント化した) 音声ファイルを出力
             ## result['end'] を使うと文の末尾が切れることがあるため、次の文の開始位置を使う
@@ -138,7 +158,7 @@ def segment(
 
             # 書き起こしのテキストファイルを出力
             with open(output_transcription_file, mode='w', encoding='utf-8') as f:
-                f.write(prepare.PrepareText(result['text']))
+                f.write(transcription)
 
             typer.echo(f'File {output_audio_file} saved.')
             count += 1
